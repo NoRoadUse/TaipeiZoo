d2f5f46651c6f193b3b76c1622b3e48b6c60ea33
johnathan <momo.johnathan@gmail.com>
2023-06-08 16:28:02 +0800

    ZooViewModel modify section and animal source to LiveData container

--- app/src/main/java/com/example/taipeizoo/viewmodel/ZooViewModel.kt
+++ app/src/main/java/com/example/taipeizoo/viewmodel/ZooViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.taipeizoo.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.taipeizoo.datamodel.AnimalData
@@ -9,28 +11,35 @@ import com.example.taipeizoo.datamodel.SectionResultX
 import com.example.taipeizoo.repository.ZooRepo
 import kotlinx.coroutines.launch
 
-class ZooViewModel(private val repo : ZooRepo = ZooRepo()) : ViewModel() {
+class ZooViewModel(private val repo: ZooRepo = ZooRepo()) : ViewModel() {
 
-    private var responseAnimal: AnimalData? = null
+    private var responseAnimal = SingleLiveEvent<AnimalData>()
     private var selectedSection: SectionResultX? = null
     private var selectedAnimal: AnimalResultX? = null
 
-    val zooSection = SingleLiveEvent<SectionData>()
+    private val _zooSection = SingleLiveEvent<SectionData>()
+    val zooSection: LiveData<SectionData>
+        get() = _zooSection
 
     fun getZooSectionIntro() {
         viewModelScope.launch {
-            zooSection.postValue(repo.getZooSectionIntro())
+            _zooSection.postValue(repo.getZooSectionIntro())
         }
     }
 
     fun getAnimalsInfo() {
         viewModelScope.launch {
-            responseAnimal = repo.getAnimalsInfo()
+            responseAnimal.value = repo.getAnimalsInfo()
         }
     }
 
+
+    private val _animalResult = MutableLiveData<AnimalResultX>()
+    val animalResult: LiveData<AnimalResultX>
+        get() = _animalResult
+
     fun getAnimals(section: String): List<AnimalResultX>? {
-        return responseAnimal?.animalResult?.results?.filter {
+        return responseAnimal.value?.animalResult?.results?.filter {
             it.aLocation == section
         }
     }
